8bc9df84fdb3e52bbf5595397e759f678b7e800d
SeunOgundipe <Sogundipe@gmail.com>
2021-11-25 00:10:36 +0100

    added the chat feed component to the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed'
 import React from 'react'
 
 const App = () => {
@@ -8,9 +9,9 @@ const App = () => {
         <ChatEngine
             height = "100vh"
             projectID = "3f7fcb4b-66c2-44a0-ac69-39318f9370b0"
-            userName = ""
-            userSecret = ""
-        
+            userName = "Simplyseun_"
+            userSecret = "123123"
+            renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} /> }
         />
     );
 }
