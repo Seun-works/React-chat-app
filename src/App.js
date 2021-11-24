8bb785b8708e00c7871f62b2a06b5cad7423d2b0
SeunOgundipe <Sogundipe@gmail.com>
2021-11-24 15:10:04 +0100

    added chat engine component and set up the foundation of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ChatEngine } from 'react-chat-engine';
+import React from 'react'
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID = "3f7fcb4b-66c2-44a0-ac69-39318f9370b0"
+            userName = ""
+            userSecret = ""
+        
+        />
+    );
 }
 
 export default App;
+
